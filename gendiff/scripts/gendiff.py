9bb8f2bd5ba5ea81c0352111964d25f259992b1e
Олег <rahilov@gmail.com>
2026-01-05 12:25:12 +0300

    3 step

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,4 +1,12 @@
 import argparse
+import json
+import os
+
+def parse_file(file_path):
+    absolute_path = os.path.abspath(file_path)
+
+    with open(absolute_path, 'r') as f:
+        return json.load(f)
 
 def main():
     parser = argparse.ArgumentParser(
@@ -17,6 +25,16 @@ def main():
     )
 
     args = parser.parse_args()
+
+    try:
+        data1 = parse_file(args.first_file)
+        data2 = parse_file(args.second_file)
+
+        print(f"Data from file 1: {data1}")
+        print(f"Data from file 2: {data2}")
+
+    except Exception as e:
+        print(f"Error reading files: {e}")
 
 if __name__ == '__main__':
     main()
